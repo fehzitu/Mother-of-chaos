5aac24974d681a68ec9d1ecc6838508d5b0a68c5
fehzitu <fehzitujsdev@gmail.com>
2026-02-16 15:55:10 -0300

    some express explanations

--- simple express/server with explanations/server.js
+++ simple express/server with explanations/server.js
@@ -1,8 +1,4 @@
 /*
-{ ORIGINAL SIMPLE HTTP SERVER WITHOUT EXPRESS }
-
-const http = require('http');
-
 const server = http.createServer((req, res) => {
     if (req.method === 'GET' && req.url === '/home') {
         res.writeHead(200, {
@@ -59,10 +55,6 @@ const server = http.createServer((req, res) => {
 });
 
 const port = 3000;
-
-server.listen(port, () => {
-    console.log(`Server running on port: http://localhost:${port}`);
-});
 */
 
 /*
@@ -71,3 +63,21 @@ const http = require('http');
 But with express we only need to use the module itself
 */
 const express = require('express');
+// And now our app will be a function to start express
+const app = express();
+// Defining a simple port
+const port = 3000;
+
+/*
+Normally in http we do it this way so that our server uses a port and serves it.
+server.listen(port, () => {
+    console.log(`Server running on port: http://localhost:${port}`);
+});
+*/
+
+// In express we do it this way.
+app.listen(port, () => {
+    console.log(`Server running on port: http://localhost:${port}`);
+});
+
+// Did you notice any difference when putting the server to listen to a port between regular http and express? Exactly! It does not have! This is one of the good reasons to learn http before express itself, express only facilitates our journey in http it does not modify it and even less makes it difficult.
